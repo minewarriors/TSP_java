8bbc00d6d1bb6bed381d1a2d0805c3317810de0a
Jelle van Urk <jellevanurk@gmail.com>
2018-05-07 14:09:01 +0200

    Retard leer te pushen en committen

--- TSP_KBS2a/src/Bruteforce/Bruteforce_test.java
+++ TSP_KBS2a/src/Bruteforce/Bruteforce_test.java
@@ -13,10 +13,7 @@ public class Bruteforce_test {
 
     public static void main(String[] args) {
         System.out.println("Hoi bram!!!");
-<<<<<<< HEAD
         System.out.println("Hoi Jelle!!! Piemelhoofd >:D");
-=======
         System.out.println("wat en jij een pannenkoek wazaaaaa!");
->>>>>>> 13ca6b4669ae5b348d2db20897790083c1cea487
     }
 }
