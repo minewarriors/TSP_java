d9c891265b6d0deafff4bed3274ebd6dfb5908b9
Jelle van Urk <jellevanurk@gmail.com>
2018-05-07 13:59:08 +0200

    Brute force package

---
+++ TSP_KBS2a/src/Bruteforce/Bruteforce_test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bruteforce;
+
+/**
+ *
+ * @author jelle
+ */
+public class Bruteforce_test {
+
+    public static void main(String[] args) {
+        System.out.println("Hoi bram!!!");
+    }
+}
