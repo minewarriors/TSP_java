6fb7c5c3969382cbed37ca93f104bc9be8957c33
Jelle van Urk <jellevanurk@gmail.com>
2018-05-08 10:55:06 +0200

    Main menu window

---
+++ TSP_KBS2a/src/Window/MainWindow.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Window;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author jelle
+ */
+public class MainWindow extends JFrame {
+
+    private JLabel JLTitle;
+    private JButton JBTSP;
+    private JButton JBBPP;
+
+    public MainWindow() {
+        //Window settings
+        setSize(1080, 720);
+        setTitle("Retrieval & Storage System");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        //Declaring Labels
+        JLTitle = new JLabel("Storage and Retrieval System MENU");
+
+        //Declaring buttons
+        JBTSP = new JButton("TSP Menu");
+        JBBPP = new JButton("BPP Menu");
+
+        //Add buttons and labels
+        add(JLTitle);
+        add(JBBPP);
+        add(JBTSP);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow Menu = new MainWindow();
+    }
+
+}
