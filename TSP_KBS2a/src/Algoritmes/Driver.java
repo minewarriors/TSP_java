de00626bf258e13bbd9d8d6c921800356a3881a1
Jelle van Urk <jellevanurk@gmail.com>
2018-05-14 15:12:41 +0200

    bruteforce finished

--- TSP_KBS2a/src/Algoritmes/Driver.java
+++ TSP_KBS2a/src/Algoritmes/Driver.java
@@ -6,6 +6,7 @@
 package Algoritmes;
 
 import Core.Product;
+import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,7 +25,9 @@ public class Driver {
             new Product(3, 3, 2, "Red", 1),
             new Product(4, 4, 4, "Red", 1),
             new Product(5, 1, 3, "Red", 1),
-            new Product(6, 3, 5, "Red", 1)
+            new Product(6, 3, 5, "Red", 1),
+            new Product(7, 1, 2, "Red", 1),
+            new Product(8, 5, 5, "Red", 1)
     ));
 
     public static void main(String[] args) {
@@ -44,15 +47,57 @@ public class Driver {
         driver.printDuration(startInstant);
     }
 
+    //print hoelang het process heeft geduurt
     public void printDuration(Instant startInstant) {
+        Duration permutationDuration = Duration.between(startInstant, Instant.now());
+        //tijd in min en sec
+        long minutes = permutationDuration.toMinutes();
+        long seconds = permutationDuration.getSeconds();
 
+        if (seconds > 59) {
+            long tempSeconds = seconds - 60 * minutes;
+            long tempMilliSeconds = permutationDuration.toMillis() - seconds * 1000;
+            System.out.println("Duration: " + minutes + " minutes " + tempSeconds + " seconds " + tempMilliSeconds + " milliseconds" + "(" + permutationDuration + ")");
+        } else if (seconds > 0) {
+            long milliSeconds = permutationDuration.toMillis() - seconds * 1000;
+            System.out.println("Duration: " + seconds + " seconds " + milliSeconds + " milliseconds" + "(" + permutationDuration + ")");
+        } else {
+            System.out.println("Duration: " + permutationDuration.toMillis() + "(" + permutationDuration + ")");
+        }
     }
 
+    //print de kortste routes
     public void printResults(BruteForce bruteForce, ArrayList<Route> shortestRoute) {
-
+        if (VERBOSE_FLAG) {
+            System.out.println("");
+        }
+        printHeading("Shortest Route", "Distance");
+        shortestRoute.stream().forEach(x -> System.out.println(x + " | " + bruteForce.getTotalDistance(x)));
     }
 
+    //maakt de kolommen netjes als permutatie van aantal producten veranderd
     public void printHeading(String headingcolumn1, String remainingHeadingColumns) {
-
+        int productNamesLength = 0;
+//        for (int x = 0; x < intialRoute.size(); x++) {
+//            productNamesLength += intialRoute.get().getProductId().length();
+//        }
+        int arrayLength = productNamesLength + intialRoute.size() * 2;
+        int partialLength = (arrayLength - headingcolumn1.length()) / 2;
+        for (int x = 0; x < partialLength; x++) {
+            System.out.println(" ");
+        }
+        System.out.println(headingcolumn1);
+        for (int x = 0; x < partialLength; x++) {
+            System.out.println(" ");
+        }
+        if ((arrayLength % 2) == 0) {
+            System.out.println(" ");
+        }
+        System.out.println(" | " + remainingHeadingColumns);
+        productNamesLength += remainingHeadingColumns.length() + 3;
+//        for (int x = 0; x < productNamesLength + intialRoute.size() * 2; x++) {
+//            System.out.println("-");
+//        }
+        System.out.println("");
     }
 }
